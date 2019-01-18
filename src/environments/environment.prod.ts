1d46c7e47cfacd384e620b3deead59260eccedeb
Maxime Gendron <gendron.maxime1@gmail.com>
2019-01-18 12:49:38 -0500

    fix envirorment for deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDnwiLpp61puNeaq_z6luI8dBH4U8HDmDk",
+    authDomain: "recipemanager-601ab.firebaseapp.com",
+    databaseURL: "https://recipemanager-601ab.firebaseio.com",
+    projectId: "recipemanager-601ab",
+    storageBucket: "recipemanager-601ab.appspot.com",
+    messagingSenderId: "814813546487"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   firebase: {
     apiKey: "AIzaSyDnwiLpp61puNeaq_z6luI8dBH4U8HDmDk",
     authDomain: "recipemanager-601ab.firebaseapp.com",
